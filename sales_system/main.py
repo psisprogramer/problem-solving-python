a2795f868ddfc441f1c9212e6c3e9c2e6ced6588
psis <carolinamolina.p15@mail.com>
2026-01-30 02:22:22 -0500

    maqueta del proyecto

---
+++ sales_system/main.py
@@ -0,0 +1,4 @@
+=== SALES SYSTEM ===
+1. Show products
+2. Make a sale
+0. Exit
---
+++ sales_system/modulos/recursos.py
@@ -0,0 +1,8 @@
+def show_products(products):
+    pass
+
+def find_product(products, product_id):
+    pass
+
+def process_sale(products):
+    pass
